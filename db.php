e5a123a8ca3b75a91f77c06acc2b7d3412490188
mtbrobotanist <floydwin7@gmail.com>
2016-05-10 00:14:45 -0700

    splitting hairs. pulled a string literal into a vairable for consistency

--- db.php
+++ db.php
@@ -5,10 +5,11 @@ function openDB(){
     try 
     {
         $server = "localhost";
-        $username = "lab4_user";
+        $dbname = "lab4";
+        $username = "lab4_user"; // note, lab4_user needs to be lab4_user@localhost inside of phpmyadmin
         $password = "Lab4!";
     
-        $conn = new PDO("mysql:host=$server;dbname=lab4", $username, $password);
+        $conn = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
