ed759e3c2029b4dcc8a7071573ebea3d3a0c354c
Bryan <bwong005@gmail.com>
2016-05-20 14:10:17 -0700

    updated files

--- lab4.php
+++ lab4.php
@@ -4,10 +4,14 @@
     
     function newUser($user, $pass){
         $conn = openDB();
-    
+        
+      //if(preg_match("#.*^(?=.{8,20})(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W).*$#", $pass)){
+            $hash = password_hash($pass, PASSWORD_DEFAULT);
+        //} 
+
         $statement = $conn->prepare("INSERT INTO users(username, password) VALUES(:user, :pass)");
         $statement->bindParam(":user", $user);
-        $statement->bindParam(":pass", $pass);
+        $statement->bindParam(":pass", $hash);
         $statement->execute();
         if($statement){
             echo "<div class='regform'><h3>Regristration completed!</h3></div>";
@@ -29,8 +33,8 @@
             echo "<div class='regform'>Invalid Password!</div>";
         }
         else{
-            $statement = $conn->prepare("SELECT username, password FROM users WHERE username=:user AND password=:pass");
-            $statement->execute(array(':user'=>$user, ':pass'=>$pass));
+            $statement = $conn->prepare("SELECT username FROM users WHERE username=:user");
+            $statement->execute(array(':user'=>$user));
             $rows = $statement->fetch(PDO::FETCH_ASSOC);
             
             if($rows['username'] == $user){
@@ -41,10 +45,35 @@
             }
         }
     }//end signup()
+    
+    function login($user, $pass){
+        $conn = openDB();
+        $statement = $conn->prepare("SELECT * FROM users WHERE username=:user LIMIT 1");
+        $statement->execute(array(':user'=>$user));
+        $userRow = $statement->fetch(PDO::FETCH_ASSOC);
+        //$sucessfull = $userRow['id'];
+        if($statement->rowCount() > 0){
+            if(password_verify($pass, $userRow['password'])){
+                echo "Welcome $user";
+                $_SESSION['username'] = $user;
+                header("Location: http://www.facebook.com");
+                return true;
+            }
+            else{
+                echo "Wrong username or password!";
+            }
+        }
+    }//end login();
 
     if(isset($_POST['register'])){
         signUp();
     }
+    else if(isset($_POST['submit'])){
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+        login($user, $pass);
+    }
+        
 ?>
 
 <html>
@@ -54,13 +83,60 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
         
         <script type="text/javascript">
+            
+            function passwordStrength(pass){
+                var score = 0;
+                if(!pass){
+                    return score;
+                }
+                
+                //award every unique letter until 5 repititions
+                var letter = new Object();
+                for (var i = 0; i < pass.length; i++) {
+                       letter[pass[i]] = (letter[pass[i]] || 0) + 1;
+                       score += 5.0 / letter[pass[i]];
+            }
+            
+               //bonus points for mixing it up
+                var variations = {
+                    digits: /\d/.test(pass),
+                    lower: /[a-z]/.test(pass),
+                    upper: /[A-Z]/.test(pass),
+                    nonWords: /\W/.test(pass),
+             }
+
+                variationCount = 0;
+                for (var check in variations) {
+                     variationCount += (variations[check] == true) ? 1 : 0;
+            }
+                score += (variationCount - 1) * 10;
+
+                 return parseInt(score);
+            }//end passwordStength();
+            
+            
+            function Strength(pass){
+                var score = passwordStrength(pass);
+                if(score > 80)
+                    return "Strong";
+                if(score > 60)
+                    return "Good";
+                if(score > 40)
+                    return "Weak";
+                return "";
+            }//end strength()
+            
             $(document).ready(function(){
-                $("#onclick").click(function(){
-                   $("#signupBox").css("display", "block");   
-             });//end onclick function to bring up display block
-                $("#exit").click(function(){
+                $("#onclick").click(function(){//will bring up display box if click
+                   $("#signupBox").css("display", "block");  
+                   $("#password").on("keypress keyup keydown", function(){
+                       var pass = $(this).val();
+                   $("#mypassword").text(Strength(pass));  
+                });//end mypassword function
+             });//end onclick function 
+                $("#exit").click(function(){//will exit the display box is cancel is hit
                     $(this).parent().parent().hide();
-                });
+                });//end exit function
             });//end document function
         </script>
     </head>
@@ -68,10 +144,11 @@
     <body class="mainpage">
         <h1>Welcome! Please log in!</h1>
         
+        
         <form method="POST">
               <div class="inputs">Username:<input type="text" name="username"/>    
                   <p>Password:<input type="password" name="password"/></p>
-                  <p><input type="submit" name="submit" value="Submit"/></p>   
+                  <p id="submitbtn"><input type="submit" name="submit" value="Submit"/></p>   
             </div>
         </form>
         
@@ -81,6 +158,9 @@
                           <h3>Register Form</h3>
                           <div class="inputs">Username: <input type="text" name="username"></div>
                           <div class="inputs">Password: <input type="password" name="password"></div>
+                          <div>
+                              Strength: <div class="intext" id="mypassword"></div>
+                          </div>
                             <input type="submit" name="register" value="Register">
                             <input type="button" id="exit" value="Cancel" />
                       </form>
